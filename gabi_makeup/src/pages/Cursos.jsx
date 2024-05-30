6d7de537cb0d2676825b30419bbed13d1512e32a
FlaviaMendesAlcantara <flavinhams23@hotmail.com>
2024-05-30 10:25:24 -0300

    tirando senha

--- gabi_makeup/src/pages/Cursos.jsx
+++ gabi_makeup/src/pages/Cursos.jsx
@@ -21,13 +21,13 @@ function Cursos() {
   useEffect(() => {
     async function fetchCursos() {
       try {
-        const response = await axios.get('cursos/');
+        const response = await axios.get('https://gabi-makeup-api-2e0d.onrender.com/v1/cursos/');
         const cursosFormatted = response.data
-        .filter(curso => curso.cur_ativo)
+        .filter(curso => curso.cur_ativo) // Filtrando apenas cursos ativos
         .map(curso => ({
           ...curso,
-          cur_data_inicio: format(new Date(curso.cur_data_inicio), 'dd/MM/yyyy'), 
-          cur_data_fim: format(new Date(curso.cur_data_fim), 'dd/MM/yyyy'), 
+          cur_data_inicio: format(new Date(curso.cur_data_inicio), 'dd/MM/yyyy'), // Formatando a data de início
+          cur_data_fim: format(new Date(curso.cur_data_fim), 'dd/MM/yyyy'), // Formatando a data de término
         }));
         setCursosData(cursosFormatted);
       } catch (error) {
@@ -47,8 +47,23 @@ function Cursos() {
     setInscricaoOpen(false);
   };
 
+  // const handleFecharModal = () => {
+  //   setModalAberto(false); // Fecha o modal
+  // };
+
   return (
     <div className="cursos-container"> 
+
+      {/* // No componente Cursos.jsx
+    <Modal show={modalAberto} onHide={handleFecharModal}>
+      <Modal.Header closeButton>
+        <Modal.Title>Criar Novo Curso</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <NovoCursoFormulario handleClose={handleFecharModal} />
+      </Modal.Body>
+    </Modal> */}
+      
       <h1>Cursos Disponíveis</h1>
       <ul className="cursos-list">
         {cursosData.map((curso) => (
@@ -97,4 +112,5 @@ function Cursos() {
     </div>
   );
 }
+
 export default Cursos;
