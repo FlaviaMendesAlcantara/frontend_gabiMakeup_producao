5d9212226b872f2c6600ddb3062f83aa7af86b4f
FlaviaMendesAlcantara <flavinhams23@hotmail.com>
2024-06-01 19:15:14 -0300

    ajustando fotos

--- gabi_makeup/src/pages/Cursos.jsx
+++ gabi_makeup/src/pages/Cursos.jsx
@@ -4,7 +4,7 @@ import { format } from 'date-fns';
 import Button from '@material-ui/core/Button';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import "../Cursos.css";
-import maquiagemIcon from "../img/autoMaquiagem.jpg"; 
+import maquiagemIcon from "../img/curso1.jpg"; 
 import InscricaoForm from "./InscricaoForm.jsx";
 import CircularProgress from '@material-ui/core/CircularProgress';
  
