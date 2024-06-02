6e41954f2ebbc210364a49bed286fc89c9124e50
FlaviaMendesAlcantara <flavinhams23@hotmail.com>
2024-06-02 10:34:46 -0300

    resolvendo acesso ao painel de cursos

--- gabi_makeup/src/pages/Cursos.jsx
+++ gabi_makeup/src/pages/Cursos.jsx
@@ -7,13 +7,10 @@ import "../Cursos.css";
 import maquiagemIcon from "../img/curso1.jpg"; 
 import InscricaoForm from "./InscricaoForm.jsx";
 import CircularProgress from '@material-ui/core/CircularProgress';
- 
-/**
- * Componente funcional que representa a página de cursos.
- * @return {JSX.Element} O componente JSX da página de cursos.
- */
+import { useAuth } from '../contexto/useAuth.jsx'; // Importe useAuth
 
 function Cursos() {
+  const { userLoggedIn } = useAuth(); // Use o hook useAuth para obter informações de autenticação do usuário
 
   const [cursosData, setCursosData] = useState([]);
   const [inscricaoOpen, setInscricaoOpen] = useState(false);
@@ -23,19 +20,18 @@ function Cursos() {
   useEffect(() => {
     async function fetchCursos() {
       try {
-
         const response = await axiosInstance.get('cursos/');
         const cursosFormatted = response.data
-        .filter(curso => curso.cur_ativo) // Filtrando apenas cursos ativos
-        .map(curso => ({
-          ...curso,
-          cur_data_inicio: format(new Date(curso.cur_data_inicio), 'dd/MM/yyyy'), // Formatando a data de início
-          cur_data_fim: format(new Date(curso.cur_data_fim), 'dd/MM/yyyy'), // Formatando a data de término
-        }));
+          .filter(curso => curso.cur_ativo)
+          .map(curso => ({
+            ...curso,
+            cur_data_inicio: format(new Date(curso.cur_data_inicio), 'dd/MM/yyyy'),
+            cur_data_fim: format(new Date(curso.cur_data_fim), 'dd/MM/yyyy'),
+          }));
         setCursosData(cursosFormatted);
       } catch (error) {
         console.error('Erro ao buscar os cursos:', error);
-      }finally {
+      } finally {
         setLoading(false); 
       }
     }
@@ -52,70 +48,70 @@ function Cursos() {
     setInscricaoOpen(false);
   };
 
-  // const handleFecharModal = () => {
-  //   setModalAberto(false); // Fecha o modal
-  // };
-
   return (
     <div className="cursos-container">       
       <h1>Cursos Disponíveis</h1>
 
-      {/* Indicador de carregamento */}
-      {loading && (
+      {userLoggedIn ? ( // Verifique se o usuário está logado
         <>
-          <div className="emoji">🤚</div>
-          <div>Carregando...</div>
-          <div className="loading-indicator">
-              <CircularProgress />
-          </div>
-        </>
-      )}
+          {loading && (
+            <>
+              <div className="emoji">🤚</div>
+              <div>Carregando...</div>
+              <div className="loading-indicator">
+                  <CircularProgress />
+              </div>
+            </>
+          )}
 
-      {!loading && (
-        <ul className="cursos-list">
-          {cursosData.map((curso) => (
-            <li key={curso.cur_id} className="curso-item">
-              <div className="curso-header">             
-                <img
-                  src={maquiagemIcon}
-                  alt="Ícone de Maquiagem"
-                  className="maquiagem-icon"
-                />             
-              </div>
-              <div className="curso-content">
-                <h3>{curso.cur_titulo}</h3>
-                <p>{curso.cur_descricao}</p>               
-                <p>
-                  <strong>Data do Curso:</strong> {curso.cur_data_inicio} até{" "}
-                  {curso.cur_data_fim}
-                </p>
-                <p>
-                  <strong>Carga Horária:</strong> {curso.cur_carga_horaria} hs
-                </p>
-                <p>
-                  <strong>Valor do investimento:</strong> R${curso.cur_valor}
-                </p>
-              </div>
+          {!loading && (
+            <ul className="cursos-list">
+              {cursosData.map((curso) => (
+                <li key={curso.cur_id} className="curso-item">
+                  <div className="curso-header">             
+                    <img
+                      src={maquiagemIcon}
+                      alt="Ícone de Maquiagem"
+                      className="maquiagem-icon"
+                    />             
+                  </div>
+                  <div className="curso-content">
+                    <h3>{curso.cur_titulo}</h3>
+                    <p>{curso.cur_descricao}</p>               
+                    <p>
+                      <strong>Data do Curso:</strong> {curso.cur_data_inicio} até{" "}
+                      {curso.cur_data_fim}
+                    </p>
+                    <p>
+                      <strong>Carga Horária:</strong> {curso.cur_carga_horaria} hs
+                    </p>
+                    <p>
+                      <strong>Valor do investimento:</strong> R${curso.cur_valor}
+                    </p>
+                  </div>
+                  
+                  <Button
+                    onClick={() => handleInscricaoOpen(curso)}
+                    variant="contained"
+                    color="secondary"
+                    endIcon={<ArrowForwardIcon />}
+                  >
+                    Inscreva-se agora
+                  </Button>
+
+                </li>
+              ))}
               
-              <Button
-                onClick={() => handleInscricaoOpen(curso)}
-                variant="contained"
-                color="secondary"
-                endIcon={<ArrowForwardIcon />}
-              >
-                Inscreva-se agora
-              </Button>
-
-            </li>
-          ))}
-          
-          {/* Modal de inscrição */}
-          <InscricaoForm
-            open={inscricaoOpen}
-            handleClose={handleInscricaoClose}
-            curso={cursoSelecionado}
-          />
-        </ul>
+              <InscricaoForm
+                open={inscricaoOpen}
+                handleClose={handleInscricaoClose}
+                curso={cursoSelecionado}
+              />
+            </ul>
+          )}
+        </>
+      ) : (
+        <p>Por favor, faça login para ver nossos cursos.</p> // Mostrar mensagem se o usuário não estiver logado
       )}
     </div>
   );
