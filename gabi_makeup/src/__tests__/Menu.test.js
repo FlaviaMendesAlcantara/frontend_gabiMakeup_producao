eca7b8f46cfddda7aa5736470dc3fcb89f0a2fd4
FlaviaMendesAlcantara <flavinhams23@hotmail.com>
2024-06-08 20:47:49 -0300

    atualizção ci/di 7

--- gabi_makeup/src/__tests__/Menu.test.js
+++ gabi_makeup/src/__tests__/Menu.test.js
@@ -3,9 +3,17 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Menu from '../componentes/Menu';
+import * as auth from '../contexto/useAuth';
 
+// Mock do hook useAuth
+jest.mock('../contexto/useAuth', () => ({
+  useAuth: jest.fn(),
+}));
 
 test('Menu renderizado sem erros', () => {
+  // Definindo o retorno mockado para useAuth
+  auth.useAuth.mockImplementation(() => ({ userLoggedIn: true, userIsAdmin: false }));
+
   render(
     <Router>
       <Menu />
